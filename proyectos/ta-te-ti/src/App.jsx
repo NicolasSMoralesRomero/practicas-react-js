2a93442f8fa98748af8e8e0fb1b9ebc7e9982621
Nicolas Morales <nicosmr56@gmail.com>
2024-08-02 12:52:33 -0300

    logica-ta-te-ti

--- proyectos/ta-te-ti/src/App.jsx
+++ proyectos/ta-te-ti/src/App.jsx
@@ -19,24 +19,79 @@ const Square =({ children, isSelected, updateBoard,index }) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 function App() {
   const [board, setBoard] = useState(
     Array(9).fill(null)
   )
   const [turn, setTurn] = useState(TURNS.X)
+  // null es que no hay ganador, false es que hay un empate
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    // revisamos todas las combinaciones ganadoras
+    // para ver si X u O ganó
+    for (const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ){
+        return boardToCheck[a]
+      }
+    }
+    // si no hay ganador
+    return null
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+  
+const checkEndGame = (newBoard) => {
+  //Revisamos si hay un empate
+  // si no hay mas espacios vacios
+  // en el tablero
+  return newBoard.every((square) => square != null)
+}
 
   const updateBoard = (index) => {
+    // no actualizamos esta posición
+    // si ya tiene algo
+    if(board[index] || winner) return
+    // actualizamos el tablero
     const newBoard = [...board] 
     newBoard[index] = turn
     setBoard(newBoard)
-    
+    // cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+    } else if(checkEndGame(newBoard)){
+      setWinner(false) //empate
+    }
   }
 
   return ( 
     <main className="board">
       <h1>Ta Te Ti</h1>
+      <button onClick={resetGame}>Reset del juego</button>
       <section className="game">
         {
           board.map((_, index) => {
@@ -61,6 +116,30 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+
+      {
+        winner != null && (
+          <section className="winner">
+            <div className="text">
+              <h2>
+                {
+                  winner === false
+                  ? "Empante"
+                  : "Gano" 
+                }
+              </h2>
+
+              <header className="win">
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
     </main>
   ) 
 }
